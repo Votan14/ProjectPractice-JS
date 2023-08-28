c4e53af268b69a8bf7ab9c2d3f45c131a243f5e2
Votan14 <51118369+Votan14@users.noreply.github.com>
2023-08-28 16:48:02 +0200

    Add FormData and XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -216,4 +216,51 @@ window.addEventListener('DOMContentLoaded', () => {
     );
 
     postnoje.createRecipe();
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Loading',
+        success: 'Thanks!',
+        failure: 'Fail!'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            form.append(statusMessage);
+            statusMessage.textContent = message.loading;
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            // request.setRequestHeader('Content-type', 'multipart/form-data');
+
+            const formData = new FormData(form);
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+
+        });
+    }
 });
